109ece62f2038a7beb0c81ce58f18c85d177fa6e
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-18 16:54:17 +0530

    Created get Profile API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\APIController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +29,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('googleauth', [AuthController::class, 'googleauth']);
      
 Route::middleware("auth:api")->group(function () {
-
+    Route::get('profile', [ProfileController::class, 'getProfile']);
     Route::prefix('getall')->group(function () {
         Route::get('horecas', [APIController::class, 'getAllHorecas']);
         Route::get('categories', [APIController::class, 'getAllCategories']);
