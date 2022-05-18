3af5fcf1aebb6fa005e4bce3a6f74430a182321c
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-18 16:29:13 +0530

    Created getAll Horecas Function

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Horeca;
+use App\Models\Category;
+use App\Models\Offer;
+use App\Models\User;
+use App\Models\City;
+use App\Models\State;
 
 use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
     //
+    public function getAllHorecas()
+    {
+        $horecas = Horeca::all();
+        return response()->json($horecas);
+    }
+
+
 }
