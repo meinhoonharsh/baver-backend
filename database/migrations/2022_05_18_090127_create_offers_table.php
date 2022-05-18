7cc9e45cd7df74f66ff0f6848eef5d3084f7d3e4
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-18 16:19:53 +0530

    Added Fields in Horecas Migration Table

--- database/migrations/2022_05_18_090127_create_offers_table.php
+++ database/migrations/2022_05_18_090127_create_offers_table.php
@@ -14,7 +14,34 @@ return new class extends Migration
     public function up()
     {
         Schema::create('offers', function (Blueprint $table) {
+
+            // Categories
+            // ID
+            // Title
+            // Location
+            // Timings
+            // Images
+            // Hero Image
+            // Start Date
+            // End Date
+            // Discount
+            // Description
+            // Note
+            // Contact
+
             $table->id();
+            $table->string('title');
+            $table->integer('category')->default(0);
+            $table->string('location')->nullable();
+            $table->string('timings')->nullable();
+            $table->json('images')->default('[]');
+            $table->string('hero_image')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->integer('discount')->default(0);
+            $table->text('description')->nullable();
+            $table->text('note')->nullable();
+            $table->string('contact')->nullable();
             $table->timestamps();
         });
     }
