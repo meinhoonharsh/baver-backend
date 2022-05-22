00e17fcc026bdb6c1fecc3ea4e5b8324ab30bc6c
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-22 17:16:37 +0530

    Updated Layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,210 +1,201 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
 
 @section('styles')
-  <style> 
-    /* Custom CSS for Page Here */
-  </style>
+    <style>
+        /* Custom CSS for Page Here */
+
+    </style>
 @endsection
 
 
 @section('scripts')
-  <script> 
-    // Custom JS for Page Here 
-  </script>
+    <script>
+        // Custom JS for Page Here
+    </script>
 @endsection
 
 @section('content')
-
-    
     <div class="row">
 
 
         <?php
-          $analytics = [
-          ["Enrolled","success"],
-          ["Upcoming","primary"],
-          ["Attended","danger"],
-          ["Attended","info"]
-        ];
-          ?>
-@foreach($analytics as $analytic)
-      <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-9">
-                <div class="d-flex align-items-center align-self-start">
-                  <h3 class="mb-0">12</h3>
-                  <p class="text-{{$analytic[1]}} ml-2 mb-0 font-weight-medium">+3.5%</p>
-                </div>
-              </div>
-              <div class="col-3">
-                <div class="icon icon-box-{{$analytic[1]}} ">
-                  <span class="mdi mdi-arrow-top-right icon-item"></span>
-                </div>
-              </div>
-            </div>
-            <h6 class="text-muted font-weight-normal">{{$analytic[0]}} growth</h6>
-          </div>
-        </div>
-      </div>
-@endforeach
-
-
-
-    </div>
-    <div class="row">
-      <div class="col-md-4 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <h4 class="card-title">Attendance Record</h4>
-            <canvas id="transaction-history" class="transaction-chart"></canvas>
-            <br>
-            <h5 class="card-title">Recently Attended</h5>
-
-            <?php
-              $attendancerecords = [1,1];
-            ?>
-            @foreach($attendancerecords as $attendance)
-            <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-1">
-              <div class="text-md-center text-xl-left">
-                <h6 class="mb-1">Introduction to JS</h6>
-                <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p>
-              </div>
-              <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-                <h6 class="font-weight-bold mb-0">$236</h6>
-              </div>
-            </div>
-            @endforeach
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-md-8 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex flex-row justify-content-between">
-              <h4 class="card-title mb-1">Ongoing Workshops</h4>
-              {{-- <p class="text-muted mb-1">Your data status</p> --}}
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <div class="preview-list">
-
-                  <?php
-                  $ongoingworkshops = ["primary", "info", "success", "danger", "warning"];
-                  ?>
-                  @foreach($ongoingworkshops as $workshop)
-                  <div class="preview-item border-bottom">
-                    <div class="preview-thumbnail">
-                      <div class="preview-icon bg-{{$workshop}}">
-                        <i class="mdi mdi-file-document"></i>
-                      </div>
-                    </div>
-                    <div class="preview-item-content d-sm-flex flex-grow">
-                      <div class="flex-grow">
-                        <h6 class="preview-subject">CRUD Operations in PHP with MySQL</h6>
-                        <p class="text-muted mb-0">By Jyoti Mathur</p>
-                      </div>
-                      <div class="mr-auto text-sm-right pt-2 pt-sm-0">
-                        <p class="text-muted">15 minutes ago</p>
-                        <p class="text-muted mb-0">30 tasks, 5 issues </p>
-                      </div>
-                    </div>
-                  </div>
-                  @endforeach
-
-
-
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <?php 
-        $workshops = [
-          ["Enrolled","success"],
-          ["Upcoming","primary"],
-          ["Attended","danger"]
-        ]
+        $analytics = [['Enrolled', 'success'], ['Upcoming', 'primary'], ['Attended', 'danger'], ['Attended', 'info']];
         ?>
-      @foreach($workshops as $workshop)
-      <div class="col-sm-4 grid-margin">
-        <div class="card">
-          <div class="card-body">
-            <h5>{{$workshop[0]}}</h5>
-            <div class="row">
-              <div class="col-8 col-sm-12 col-xl-8 my-auto">
-                <div class="d-flex d-sm-block d-md-flex align-items-center">
-                  <h2 class=" h1 mb-0">12</h2>
-                  {{-- <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p> --}}
+        @foreach ($analytics as $analytic)
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    <h3 class="mb-0">12</h3>
+                                    <p class="text-{{ $analytic[1] }} ml-2 mb-0 font-weight-medium">+3.5%</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-{{ $analytic[1] }} ">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">{{ $analytic[0] }} growth</h6>
+                    </div>
                 </div>
-                <h6 class="text-muted h3 font-weight-normal">Workshops</h6>
-              </div>
-              <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                <i class="icon-lg mdi mdi-codepen text-{{$workshop[1]}} ml-auto"></i>
-              </div>
             </div>
-          </div>
+        @endforeach
+
+
+
+    </div>
+    <div class="row">
+        <div class="col-md-4 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Attendance Record</h4>
+                    <canvas id="transaction-history" class="transaction-chart"></canvas>
+                    <br>
+                    <h5 class="card-title">Recently Attended</h5>
+
+                    <?php
+                    $attendancerecords = [1, 1];
+                    ?>
+                    @foreach ($attendancerecords as $attendance)
+                        <div
+                            class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-1">
+                            <div class="text-md-center text-xl-left">
+                                <h6 class="mb-1">Introduction to JS</h6>
+                                <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                            </div>
+                            <div
+                                class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                                <h6 class="font-weight-bold mb-0">$236</h6>
+                            </div>
+                        </div>
+                    @endforeach
+
+
+                </div>
+            </div>
         </div>
-      </div>
-      @endforeach
+        <div class="col-md-8 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex flex-row justify-content-between">
+                        <h4 class="card-title mb-1">Ongoing Workshops</h4>
+                        {{-- <p class="text-muted mb-1">Your data status</p> --}}
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="preview-list">
+
+                                <?php
+                                $ongoingworkshops = ['primary', 'info', 'success', 'danger', 'warning'];
+                                ?>
+                                @foreach ($ongoingworkshops as $workshop)
+                                    <div class="preview-item border-bottom">
+                                        <div class="preview-thumbnail">
+                                            <div class="preview-icon bg-{{ $workshop }}">
+                                                <i class="mdi mdi-file-document"></i>
+                                            </div>
+                                        </div>
+                                        <div class="preview-item-content d-sm-flex flex-grow">
+                                            <div class="flex-grow">
+                                                <h6 class="preview-subject">CRUD Operations in PHP with MySQL</h6>
+                                                <p class="text-muted mb-0">By Jyoti Mathur</p>
+                                            </div>
+                                            <div class="mr-auto text-sm-right pt-2 pt-sm-0">
+                                                <p class="text-muted">15 minutes ago</p>
+                                                <p class="text-muted mb-0">30 tasks, 5 issues </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <?php
+        $workshops = [['Enrolled', 'success'], ['Upcoming', 'primary'], ['Attended', 'danger']];
+        ?>
+        @foreach ($workshops as $workshop)
+            <div class="col-sm-4 grid-margin">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>{{ $workshop[0] }}</h5>
+                        <div class="row">
+                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                                <div class="d-flex d-sm-block d-md-flex align-items-center">
+                                    <h2 class=" h1 mb-0">12</h2>
+                                    {{-- <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p> --}}
+                                </div>
+                                <h6 class="text-muted h3 font-weight-normal">Workshops</h6>
+                            </div>
+                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                                <i class="icon-lg mdi mdi-codepen text-{{ $workshop[1] }} ml-auto"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
     <div class="row ">
-      <div class="col-12 grid-margin">
-        <div class="card">
-          <div class="card-body">
-            <h4 class="card-title">Order Status</h4>
-            <div class="table-responsive">
-              <table class="table ">
-                <thead>
-                  <tr>
-                    <th>
-                      #
-                    </th>
-                    <th> Workshop</th>
-                    <th> Host by</th>
-                    <th> On Date</th>
-                    <th> Started at</th>
-                    <th> Ended at</th>
-                  </tr>
-                </thead>
-                <tbody>
+        <div class="col-12 grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Order Status</h4>
+                    <div class="table-responsive">
+                        <table class="table ">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        #
+                                    </th>
+                                    <th> Workshop</th>
+                                    <th> Host by</th>
+                                    <th> On Date</th>
+                                    <th> Started at</th>
+                                    <th> Ended at</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
 
-                  <?php 
-                    $completedworkshops = [1,1,1,1,1,1]
-                    ?>
-                  @foreach($completedworkshops as $workshop)
-                  <tr>
-                    <td>
-                      {{$loop->iteration}}
-                    </td>
-                    <td class="text-light font-weight-medium">
-                      Introduction to Designing
-                    </td>
-                    <td> Jyoti Mathur </td>
-                    <td> 28 July 2021 </td>
-                    <td> 18:00 </td>
-                    <td> 19:00 </td>
-                  </tr>
-
-                  @endforeach
-
+                                <?php
+                                $completedworkshops = [1, 1, 1, 1, 1, 1];
+                                ?>
+                                @foreach ($completedworkshops as $workshop)
+                                    <tr>
+                                        <td>
+                                            {{ $loop->iteration }}
+                                        </td>
+                                        <td class="text-light font-weight-medium">
+                                            Introduction to Designing
+                                        </td>
+                                        <td> Jyoti Mathur </td>
+                                        <td> 28 July 2021 </td>
+                                        <td> 18:00 </td>
+                                        <td> 19:00 </td>
+                                    </tr>
+                                @endforeach
 
 
-                </tbody>
-              </table>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
 
 
@@ -438,6 +429,4 @@
         </div>
       </div>
     </div> --}}
-
-
 @endsection
