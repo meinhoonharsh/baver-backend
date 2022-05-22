744f8f385d446729cff19b6a12ed327a4a7f9a61
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-22 14:44:31 +0530

    Created All Get all Functions

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -13,10 +13,34 @@ use Illuminate\Http\Request;
 class APIController extends Controller
 {
     //
-    public function getAllHorecas()
-    {
+    public function getAllHorecas(){
         $horecas = Horeca::where('active', 1)->get();
         return response()->json($horecas);
+    }
+
+    public function getAllCategories(){
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    public function getAllOffers(){
+        $offers = Offer::all();
+        return response()->json($offers);
+    }
+
+    public function getAllUsers(){
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function getAllCities(){
+        $cities = City::all();
+        return response()->json($cities);
+    }
+
+    public function getAllStates(){
+        $states = State::all();
+        return response()->json($states);
     }
 
 
