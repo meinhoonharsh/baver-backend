269e59c6501b048ac780cfc5deaf229a8f3ffe14
meinhoonharsh <harshprogrammer782@gmail.com>
2022-06-08 01:19:12 +0530

    created mobileheader partial

---
+++ resources/views/layouts/partials/mobileheader.blade.php
@@ -0,0 +1,136 @@
+<div id="apus-mobile-menu" class="apus-offcanvas hidden-lg">
+    <button type="button" class="btn btn-toggle-canvas" data-toggle="offcanvas">
+        <i class="ti-close"></i>
+    </button>
+    <div class="apus-offcanvas-body">
+        <div class="header-top-mobile clearfix">
+            <div class="add-listing">
+                <a class="btn btn-addlisting btn-theme" href="submit-listing/index.htm"><i class="ti-plus"
+                        aria-hidden="true"></i> Add Listing</a>
+            </div>
+        </div>
+        <nav class="navbar navbar-offcanvas navbar-static" role="navigation">
+            <div class="navbar-collapse navbar-offcanvas-collapse">
+                <ul id="main-mobile-menu" class="nav navbar-nav">
+                    <li id="menu-item-66" class="has-submenu active menu-item-66"><a href="#">Home</a> <span
+                            class="icon-toggle"><i class="ti-angle-down"></i></span>
+                        <ul class="sub-menu">
+                            <li id="menu-item-75" class="active menu-item-75"><a href="index.htm">Home 1</a>
+                            </li>
+                            <li id="menu-item-73" class="menu-item-73"><a href="home-2/index.htm">Home 2</a>
+                            </li>
+                            <li id="menu-item-72" class="menu-item-72"><a href="home-3/index.htm">Home 3</a>
+                            </li>
+                            <li id="menu-item-71" class="menu-item-71"><a href="home-4/index.htm">Home 4</a>
+                            </li>
+                            <li id="menu-item-251" class="menu-item-251"><a href="home-5/index.htm">Home 5</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-307" class="has-submenu menu-item-307"><a href="listings/index.htm">Listings</a>
+                        <span class="icon-toggle"><i class="ti-angle-down"></i></span>
+                        <ul class="sub-menu">
+                            <li id="menu-item-621" class="menu-item-621"><a href="listing-full/index.htm">Listings
+                                    &#8211; Full</a></li>
+                            <li id="menu-item-615" class="menu-item-615"><a
+                                    href="listing-with-sidebar/index.htm">Listings &#8211; Sidebar</a></li>
+                            <li id="menu-item-616" class="menu-item-616"><a href="listing-half-map/index.htm">Listings
+                                    &#8211; Half Map</a></li>
+                            <li id="menu-item-617" class="menu-item-617"><a href="listing-list/index.htm">Listings
+                                    &#8211; List</a></li>
+                            <li id="menu-item-618" class="menu-item-618"><a
+                                    href="listing/spotlight-club/index.htm">Listing Detail</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-70" class="has-submenu menu-item-70"><a href="blog/index.htm">Blog</a>
+                        <span class="icon-toggle"><i class="ti-angle-down"></i></span>
+                        <ul class="sub-menu">
+                            <li id="menu-item-623" class="menu-item-623"><a
+                                    href="blog/index-1.htm?_preset=1580455841">Blog &#8211; Sidebar</a></li>
+                            <li id="menu-item-624" class="menu-item-624"><a
+                                    href="blog/index-2.htm?_preset=1580455912">Blog &#8211; Full</a></li>
+                            <li id="menu-item-622" class="menu-item-622"><a
+                                    href="blog/index-3.htm?_preset=1580455697">Blog &#8211; List</a></li>
+                            <li id="menu-item-625" class="menu-item-625"><a
+                                    href="why-people-use-listing-hub/index.htm">Blog Detail</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-76" class="has-submenu menu-item-76"><a href="#">Pages</a> <span
+                            class="icon-toggle"><i class="ti-angle-down"></i></span>
+                        <ul class="sub-menu">
+                            <li id="menu-item-1387" class="menu-item-1387"><a href="shop/index.htm">Shop</a>
+                            </li>
+                            <li id="menu-item-67" class="menu-item-67"><a href="contact/index.htm">Contact</a>
+                            </li>
+                            <li id="menu-item-69" class="menu-item-69"><a href="pricing/index.htm">Pricing</a>
+                            </li>
+                            <li id="menu-item-68" class="menu-item-68"><a href="404-2/index.htm">404</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+<div class="over-dark"></div>
+<div id="apus-header-mobile" class="header-mobile hidden-lg clearfix">
+    <div class="container">
+        <div class="row flex-middle">
+            <div class="col-xs-7 left-inner">
+                <div class="flex-middle">
+                    <div class="active-mobile">
+                        <button data-toggle="offcanvas" class="btn btn-sm btn-offcanvas offcanvas" type="button">
+                            <i class="ti-align-left" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                    <div class="logo logo-theme">
+                        <a href="index.htm">
+                            <img src="/assets/findus/wp-content/themes/findus/images/logo.png" alt="Findus">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-5">
+                <div class="flex-middle">
+                    <div class="ali-right">
+                        <div class="apus-top-cart cart">
+                            <a class="dropdown-toggle mini-cart" href="#" title="View your shopping cart">
+                                <i class="ti-shopping-cart"></i>
+                                <span class="count">0</span>
+                                <span class="total-minicart hidden"><span class="woocommerce-Price-amount amount"><span
+                                            class="woocommerce-Price-currencySymbol">&#36;</span>0</span></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <div class="widget_shopping_cart_content">
+                                    <div class="shopping_cart_content">
+                                        <div class="cart_list ">
+
+
+                                            <p class="total empty">Currently Empty: <span
+                                                    class="woocommerce-Price-amount amount"><span
+                                                        class="woocommerce-Price-currencySymbol">&#36;</span>0</span>
+                                            </p>
+                                            <div class="buttons clearfix">
+                                                <a href="shop/index.htm"
+                                                    class="btn btn-block btn-primary wc-forward">Continue
+                                                    shopping</a>
+                                            </div>
+                                        </div><!-- end product list -->
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="account-login">
+                            <ul class="login-account">
+                                <li class="icon-log"><a href="#apus_login_forgot_tab"
+                                        class="apus-user-login wel-user"><i class="ti-user"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
